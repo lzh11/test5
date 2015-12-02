e2311d851a76346cb4307d5ea478096fdb48d5b9
lzh11 <2239040999@qq.com>
2015-12-02 21:22:52 +0800

    test5
    指针的应用

--- test5/main.cpp
+++ test5/main.cpp
@@ -7,9 +7,34 @@
 //
 
 #include <iostream>
+struct student
+{
+    char name[100];
+    int age;
+    student *next;
+};
 
 int main(int argc, const char * argv[]) {
     // insert code here...
+    int i;
+    student *p,*q,*head;
+    head=new student;
+    head->next=0;
+    p=head;
+    for(i=0;i<4;i++)
+    {
+        q=new student;
+        p->next=q;
+        q->next=0;
+        scanf("%s %d",q->name,&(q->age));
+        p=q;
+    }
+    p=head->next;
+    while(p!=0)
+    {
+        printf("%s %d p=%x\n",p->name,p->age,p);
+        p=p->next;
+    }
     std::cout << "Hello, World!\n";
     return 0;
 }
