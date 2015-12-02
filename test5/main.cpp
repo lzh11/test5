ebcb005fd71158f3df36a2426619a27609671d80
lzh11 <2239040999@qq.com>
2015-12-02 20:50:46 +0800

    Initial Commit

---
+++ test5/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  test5
+//
+//  Created by LZH on 15/12/2.
+//  Copyright © 2015年 LZH. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
